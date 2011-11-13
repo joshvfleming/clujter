fd5791100e2374fb942af4e4591da9cace06d525
joshvfleming <jf@joshfleming.com>
2011-11-13 11:14:18 -0800

    Got k-means working. Yay!

--- test/clujter/test/core.clj
+++ test/clujter/test/core.clj
@@ -2,5 +2,20 @@
   (:use [clujter.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(def ^:dynamic *test-data-1* [[1 1] [22 15] [1 2] [23 25] [2 4]])
+(def ^:dynamic *test-data-1-clustered*
+     {[22.5 20.0] [[22 15] [23 25]],
+      [1.3333334 2.3333333] [[1 1] [1 2] [2 4]]})
+
+(defn point-set
+  [clusters]
+  (set (map #(set (last %)) clusters)))
+
+(defn clusters-equal
+  [c1 c2]
+  (= (point-set c1) (point-set c2)))
+
+(deftest k-cluster-test
+  (let [clusters (k-cluster *test-data-1* 2)]
+    (is (= (count clusters) 2))
+    (is (clusters-equal *test-data-1-clustered* clusters))))
