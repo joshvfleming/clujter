d2d24925e14d8c831f30a56e30283fa1fe03c9a0
joshvfleming <jf@joshfleming.com>
2012-03-08 18:42:57 -0800

    Put file read in a with-open to ensure proper closing.

--- src/clujter/core.clj
+++ src/clujter/core.clj
@@ -29,15 +29,15 @@
 (defn read-data-file
   "Reads a data file, returning an entity count and data points."
   [filename]
-  (let [reader (-> filename FileReader. BufferedReader.)
-        lines (line-seq reader)
-        read-line (fn [line]
-                    (map #(Float/parseFloat %)
-                         (str/split (str/trim line) #"\s")))
-        out (assoc {} :count
-                   (Integer/parseInt (first lines)))]
-    (assoc out :data
-           (map read-line (rest lines)))))
+  (with-open [reader (-> filename FileReader. BufferedReader.)]
+    (let [lines (doall (line-seq reader))
+          read-line (fn [line]
+                      (map #(Float/parseFloat %)
+                           (str/split (str/trim line) #"\s")))
+          out (assoc {} :count
+                     (Integer/parseInt (first lines)))]
+      (assoc out :data
+             (map read-line (rest lines))))))
 
 (defn print-results
   "Prints the results of a clustering run."
