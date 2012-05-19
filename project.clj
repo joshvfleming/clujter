4193e04a9b7682dee5ef4878f790bb0dd63326cd
joshvfleming <jf@joshfleming.com>
2012-05-18 17:11:56 -0700

    Bumped versions of clojure and midje.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clujter "1.0.0-SNAPSHOT"
   :description "Clujter contains basic implementations of unsupervised learning techniques."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [midje "1.3.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [midje "1.4.0"]]
   :main clujter.cli)
