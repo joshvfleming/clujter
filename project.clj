d20d2e7b86e5e870b0d17d81633937f689bead21
joshvfleming <jf@joshfleming.com>
2012-05-20 21:42:24 -0700

    Added optimize lib with a stub for gradient descent.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clujter "1.0.0-SNAPSHOT"
   :description "Clujter contains basic implementations of unsupervised learning techniques."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [midje "1.4.0"]]
+                 [midje "1.4.0"]
+                 [incanter "1.3.0"]]
   :main clujter.cli)
---
+++ src/clujter/optimize.clj
@@ -0,0 +1,34 @@
+(ns clujter.optimize
+  (:use incanter.core))
+
+(defn gradient-descent
+  "Performs gradient descent, returning the learned theta and cost history."
+  [cost-f X y theta alpha num-iters]
+  (loop [theta theta
+         i num-iters
+         cost-history []]
+    (let [[J grad] (cost-f X y theta)
+          theta (minus theta (mult alpha grad))
+          cost-history (conj cost-history J)]
+      (if (= i 1)
+        [theta cost-history]
+        (recur theta (dec i) cost-history)))))
+
+(defn sigmoid
+  "Sigmoid function."
+  [x]
+  (let [f (fn [t] (/ 1 (+ 1 (Math/exp (- x)))))
+        matrix-f (fn [t] (div 1 (plus 1 (exp (mult x -1)))))]
+    (cond (matrix? x) (matrix-f x)
+          (coll? x) (map f x)
+          :else (f x))))
+
+(defn sigmoid-gradient
+  "Sigmoid gradient."
+  [x]
+  (mult (sigmoid x) (minus 1 (sigmoid x))))
+
+(defn nn-cost
+  "Simple neural network cost function."
+  [X y theta lambda labels]
+  true)
